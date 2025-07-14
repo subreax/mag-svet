c6d0430c24bbbaa41057c93382013a2a704e9b02
subreax <dima2000947@gmail.com>
2025-07-14 14:30:08 +0300

    Добавление swiper для прокручиваемых списков

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 4,
+  spaceBetween: 48
+});
+
+/* const swiper_5 = new Swiper('.swiper-5', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  slidesPerView: 5,
+  spaceBetween: 48
+}); */
